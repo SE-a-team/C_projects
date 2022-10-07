14aaf3bfb1d432bbba236256c0e63c105c3e126c
Edozie <odigweedozie2@gmail.com>
2022-10-07 22:20:39 +0000

    I created a new folder inside Tony folder, that contains tests for each task that was given, and so far task 0 and 1 failed coupled with betty check

---
+++ Tony/tony_tests/Arguments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>  
+
+/**
+ * main - entry point
+ *
+ * Return :always return 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+ 
+
+   printf("Number of Arguments:");
+        printf(" %d\n", argc-1);
+
+return 0;
+}
---
+++ Tony/tony_tests/main.h
@@ -0,0 +1,10 @@
+#ifndef TONY_H
+#define TONY_H
+
+int main(void);
+int print_squareroot(int n);
+void update(int *n);
+
+
+
+#endif
