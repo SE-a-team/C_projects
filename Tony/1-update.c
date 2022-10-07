076a228a2fe1aa94f6a98001556b0abac9bd9306
Anthony Olayemi <thonyfrancis8669@gmail.com>
2022-10-07 20:54:45 +0000

    done all task, except task 2

---
+++ Tony/1-update.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "tony.h"
+
+/**
+ * main - Entry point
+ *
+ * Return - always 0
+ */
+
+int main(void) {
+
+	int n = 1000;
+	printf("n = %d\n", n);
+	{
+	void update(int *n);
+int n = 50;
+
+	printf("n = %d\n", n);
+	
+	}
+}
